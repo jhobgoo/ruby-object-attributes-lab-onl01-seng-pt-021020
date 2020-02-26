02eea61c4ca6993867fcf528d8384a7fcdac4fd4
Joel Hobgood <jhobgoo@gmail.com>
2020-02-26 03:09:57 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+  def name=(persons_name)
+    @this_persons_name = dogs_name
+  end
+  
+  def name
+    @this_dogs_name
+  end
+  
+  def breed=(dogs_breed)
+    @this_dogs_breed = dogs_breed
+  end
+  
+  def breed
+    @this_dogs_breed
+  end
+  
+end
