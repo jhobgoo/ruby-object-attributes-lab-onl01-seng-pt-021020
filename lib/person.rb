5e6205cf72273ace72152610ebc1a859ce81f3fe
Joel Hobgood <jhobgoo@gmail.com>
2020-02-26 03:11:39 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,19 +1,19 @@
 class Person
 
   def name=(persons_name)
-    @this_persons_name = dogs_name
+    @this_persons_name = persons_name
   end
   
   def name
-    @this_dogs_name
+    @this_persons_name
   end
   
-  def breed=(dogs_breed)
-    @this_dogs_breed = dogs_breed
+  def job=(persons_job)
+    @this_persons_job = persons_job
   end
   
-  def breed
-    @this_dogs_breed
+  def job
+    @this_persons_job
   end
   
 end
